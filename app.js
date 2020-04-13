717016e0e6fd04afea83fb4ec5b8b98e087a9754
Your Name <you@example.com>
2020-04-13 05:35:18 -0400

    test

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert("test");
+const defaultconfig = {
+    // apiKey: 'AIzaSyAAjpX__jctYv_EB0DLr07zZN9u0Z5IBMA',
+    apiKey: "test",
+    authDomain: "onepad.github.io",
+    databaseURL: "https://onepadeditor.firebaseio.com"
+};
+
+function btnConfig(config) {
+    if (config == undefined) {
+        config = defaultconfig;
+    }
+    Cookies.set("fconf", JSON.stringify(config));
+    console.log("config set");
+    console.log(config)
+}
