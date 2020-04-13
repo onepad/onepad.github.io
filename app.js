8519543b0b06d91bafc5696263da36595a31a784
Your Name <you@example.com>
2020-04-13 05:49:05 -0400

    test

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const defaultconfig = {
 };
 
 function btnConfig(config) {
+    $("#overlay").show();
     if (config == undefined) {
         config = defaultconfig;
     }
     Cookies.set("fconf", JSON.stringify(config));
     console.log("config set");
     console.log(config)
+}
+
+function btnSetConfigForm() {
+$("body").append("<div class=\"firepad-dialog\" id=\"overlay\"><div class=\"firepad-dialog-div\"><input class=\"firepad-dialog-input\" id=\"img\" type=\"text\" placeholder=\"Insert Firebase Api Key\" autofocus=\"autofocus\"><div class=\"firepad-btn-group\"><a class=\"firepad-btn\" id=\"submitbtn\">Submit</a><a class=\"firepad-btn\">Cancel</a></div></div></div>")
 }
